dd8aa6259878c09fae3ae18bede0bf7080f21f99
Michael Adams <macmadams@gmail.com>
2013-12-31 00:39:22 -0500

    seeing if I can alter the button on click

--- convoskew.js
+++ convoskew.js
@@ -1,3 +1,4 @@
 function skewConvo() {
 document.getElementById('result').innerHTML="<textarea placeholder='Your code will eventually go here.'></textarea>";
+document.getElementById('submitbutton').innerHTML="Further clicking will not help.";
 }
